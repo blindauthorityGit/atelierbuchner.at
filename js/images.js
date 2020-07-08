2892859f8b12ea181eb0b68234fd0f0b530253b0
Johannes <office@atelierbuchner.at>
2020-07-08 16:00:57 +0200

    first commit

---
+++ js/images.js
@@ -0,0 +1,571 @@
+let dawn = [
+    {
+        name: "Penguin Dance",
+        kategorie: "Dawn",
+        link: "../img/bilder/dawn_1.jpg",
+        overlay: "../img/overlay/dawn_1.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "150cm x 200cm",
+        index: 0,
+    },
+    {
+        name: "Just arrived",
+        kategorie: "Dawn",
+        link: "../img/bilder/dawn_2.jpg",
+        overlay: "../img/overlay/dawn_2.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "150cm x 180cm",
+        index: 1,
+    },
+    {
+        name: "Birdwalk",
+        kategorie: "Dawn",
+        link: "../img/bilder/dawn_3.jpg",
+        overlay: "../img/overlay/dawn_3.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "150cm x 180cm",
+        index: 2,
+    },
+    {
+        name: " Blue Birds Crossing",
+        kategorie: "Dawn",
+        link: "../img/bilder/dawn_4.jpg",
+        overlay: "../img/overlay/dawn_4.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "150cm x 180cm",
+        index: 3,
+    },
+    {
+        name: "Touch",
+        kategorie: "Dawn",
+        link: "../img/bilder/dawn_5.jpg",
+        overlay: "../img/overlay/dawn_5.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "120cm x 150cm",
+        index: 4,
+    },
+    {
+        name: "On the beach",
+        kategorie: "Dawn",
+        link: "../img/bilder/dawn_6.jpg",
+        overlay: "../img/overlay/dawn_6.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "100cm x 100cm",
+        index: 5,
+    },
+    {
+        name: "Paperplane Pilots",
+        kategorie: "Dawn",
+        link: "../img/bilder/dawn_7.jpg",
+        overlay: "../img/overlay/dawn_7.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "100cm x 150cm",
+        index: 6,
+    },
+    {
+        name: "Sleeping Ferryman",
+        kategorie: "Dawn",
+        link: "../img/bilder/dawn_8.jpg",
+        overlay: "../img/overlay/dawn_8.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "100cm x 120cm",
+        index: 7,
+    },
+    {
+        name: "Gods in Love",
+        kategorie: "Dawn",
+        link: "../img/bilder/dawn_9.jpg",
+        overlay: "../img/overlay/dawn_9.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "100cm x 120cm",
+        index: 8,
+    },
+];
+
+let magnificent = [
+    {
+        name: "Magificent",
+        kategorie: "Magnificent",
+        link: "../img/bilder/magnificent_1.jpg",
+        overlay: "../img/overlay/magnificent_1.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "100cm x 120cm",
+        index: 0,
+    },
+    {
+        name: "Magificent",
+        kategorie: "Magnificent",
+        link: "../img/bilder/magnificent_2.jpg",
+        overlay: "../img/overlay/magnificent_2.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "100cm x 120cm",
+        index: 1,
+    },
+    {
+        name: "Magificent",
+        kategorie: "Magnificent",
+        link: "../img/bilder/magnificent_3.jpg",
+        overlay: "../img/overlay/magnificent_3.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "100cm x 120cm",
+        index: 2,
+    },
+    {
+        name: "Magificent",
+        kategorie: "Magnificent",
+        link: "../img/bilder/magnificent_4.jpg",
+        overlay: "../img/overlay/magnificent_4.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "100cm x 120cm",
+        index: 3,
+    },
+];
+
+let garden = [
+    {
+        name: "Blue Poppie",
+        kategorie: "Garden",
+        link: "../img/bilder/garden_1.jpg",
+        overlay: "../img/overlay/garden_1.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "100cm x 120cm",
+        index: 0,
+    },
+    {
+        name: "Rose",
+        kategorie: "Garden",
+        link: "../img/bilder/garden_2.jpg",
+        overlay: "../img/overlay/garden_2.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "100cm x 120cm",
+        index: 1,
+    },
+    {
+        name: "Harlekin",
+        kategorie: "Garden",
+        link: "../img/bilder/garden_3.jpg",
+        overlay: "../img/overlay/garden_3.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "100cm x 120cm",
+        index: 2,
+    },
+    {
+        name: "What's up",
+        kategorie: "Garden",
+        link: "../img/bilder/garden_4.jpg",
+        overlay: "../img/overlay/garden_4.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "90cm x 90cm",
+        index: 3,
+    },
+];
+
+let heat = [
+    {
+        name: "Dance",
+        kategorie: "Heat",
+        link: "../img/bilder/heat_1.jpg",
+        overlay: "../img/overlay/heat_1.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "120cm x 150cm",
+        index: 0,
+    },
+    {
+        name: "In the Jungle",
+        kategorie: "Heat",
+        link: "../img/bilder/heat_2.jpg",
+        overlay: "../img/overlay/heat_2.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "120cm x 180cm",
+        index: 1,
+    },
+    {
+        name: "Waiting",
+        kategorie: "Heat",
+        link: "../img/bilder/heat_3.jpg",
+        overlay: "../img/overlay/heat_3.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "120cm x 180cm",
+        index: 2,
+    },
+    {
+        name: "Veins",
+        kategorie: "Heat",
+        link: "../img/bilder/heat_4.jpg",
+        overlay: "../img/overlay/heat_4.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "120cm x 180cm",
+        index: 3,
+    },
+];
+
+let nature = [
+    {
+        name: "Trees",
+        kategorie: "Nature",
+        link: "../img/bilder/nature_1.jpg",
+        overlay: "../img/overlay/nature_1.jpg",
+        stil: "Pigment & Bienenwachs",
+        size: "100cm x 100cm",
+        index: 0,
+    },
+    {
+        name: "Trees",
+        kategorie: "Nature",
+        link: "../img/bilder/nature_2.jpg",
+        overlay: "../img/overlay/nature_2.jpg",
+        stil: "Pigment & Bienenwachs",
+        size: "100cm x 100cm",
+        index: 1,
+    },
+    {
+        name: "Landscape",
+        kategorie: "Nature",
+        link: "../img/bilder/nature_3.jpg",
+        overlay: "../img/overlay/nature_3.jpg",
+        stil: "Pigment & Bienenwachs",
+        size: "100cm x 100cm",
+        index: 2,
+    },
+    {
+        name: "Venice",
+        kategorie: "Nature",
+        link: "../img/bilder/nature_4.jpg",
+        overlay: "../img/overlay/nature_4.jpg",
+        stil: "Pigment & Bienenwachs",
+        size: "80cm x 120cm",
+        index: 3,
+    },
+    {
+        name: "Trees",
+        kategorie: "Nature",
+        link: "../img/bilder/nature_5.jpg",
+        overlay: "../img/overlay/nature_5.jpg",
+        stil: "Pigment & Bienenwachs",
+        size: "100cm x 100cm",
+        index: 4,
+    },
+    {
+        name: "River Meadows",
+        kategorie: "Nature",
+        link: "../img/bilder/nature_6.jpg",
+        overlay: "../img/overlay/nature_6.jpg",
+        stil: "Pigment & Bienenwachs",
+        size: "50cm x 50cm",
+        index: 5,
+    },
+    {
+        name: "Trees",
+        kategorie: "Nature",
+        link: "../img/bilder/nature_7.jpg",
+        overlay: "../img/overlay/nature_7.jpg",
+        stil: "Pigment & Bienenwachs",
+        size: "100cm x 100cm",
+        index: 6,
+    },
+    {
+        name: "River Meadows",
+        kategorie: "Nature",
+        link: "../img/bilder/nature_8.jpg",
+        overlay: "../img/overlay/nature_8.jpg",
+        stil: "Pigment & Bienenwachs",
+        size: "50cm x 50cm",
+        index: 7,
+    },
+    {
+        name: "Venice",
+        kategorie: "Nature",
+        link: "../img/bilder/nature_9.jpg",
+        overlay: "../img/overlay/nature_9.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "80cm x 100cm",
+        index: 8,
+    },
+    {
+        name: "Venice",
+        kategorie: "Nature",
+        link: "../img/bilder/nature_10.jpg",
+        overlay: "../img/overlay/nature_10.jpg",
+        stil: "Pigment & Bienenwachs",
+        size: "80cm x 100cm",
+        index: 9,
+    },
+    {
+        name: "Venice",
+        kategorie: "Nature",
+        link: "../img/bilder/nature_11.jpg",
+        overlay: "../img/overlay/nature_11.jpg",
+        stil: "Pigment & Bienenwachs",
+        size: "80cm x 100cm",
+        index: 10,
+    },
+    {
+        name: "Mountains",
+        kategorie: "Nature",
+        link: "../img/bilder/nature_12.jpg",
+        overlay: "../img/overlay/nature_12.jpg",
+        stil: "Pigment & Bienenwachs",
+        size: "40cm x 40cm",
+        index: 11,
+    },
+];
+
+let layers = [
+    {
+        name: "Faces",
+        kategorie: "Layers",
+        link: "../img/bilder/layers_1.jpg",
+        overlay: "../img/overlay/layers_1.jpg",
+        stil: "Acryl & Kohle",
+        size: "110cm x 150cm",
+        index: 0,
+    },
+    {
+        name: "Dance",
+        kategorie: "Layers",
+        link: "../img/bilder/layers_2.jpg",
+        overlay: "../img/overlay/layers_2.jpg",
+        stil: "Acryl & Kohle",
+        size: "110cm x 150cm",
+        index: 1,
+    },
+    {
+        name: "Baloon",
+        kategorie: "Layers",
+        link: "../img/bilder/layers_3.jpg",
+        overlay: "../img/overlay/layers_3.jpg",
+        stil: "Acryl & Kohle",
+        size: "110cm x 130cm",
+        index: 2,
+    },
+    {
+        name: "Visions",
+        kategorie: "Layers",
+        link: "../img/bilder/layers_4.jpg",
+        overlay: "../img/overlay/layers_4.jpg",
+        stil: "Acryl & Kohle",
+        size: "110cm x 120cm",
+        index: 3,
+    },
+    {
+        name: "Dreaming Plants",
+        kategorie: "Layers",
+        link: "../img/bilder/layers_5.jpg",
+        overlay: "../img/overlay/layers_5.jpg",
+        stil: "Acryl & Kohle",
+        size: "110cm x 120cm",
+        index: 4,
+    },
+    {
+        name: "History",
+        kategorie: "Layers",
+        link: "../img/bilder/layers_6.jpg",
+        overlay: "../img/overlay/layers_6.jpg",
+        stil: "Acryl & Kohle",
+        size: "110cm x 240cm",
+        index: 5,
+    },
+];
+let rainySpring = [
+    {
+        name: "Nat & Gonba",
+        kategorie: "Rainy Spring",
+        link: "../img/bilder/rainyspring_1.jpg",
+        overlay: "../img/overlay/rainyspring_1.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "150cm x 180cm",
+        index: 0,
+    },
+    {
+        name: "Rainy Spring",
+        kategorie: "Rainy Spring",
+        link: "../img/bilder/rainyspring_2.jpg",
+        overlay: "../img/overlay/rainyspring_2.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "150cm x 180cm",
+        index: 1,
+    },
+    {
+        name: "Malach",
+        kategorie: "Rainy Spring",
+        link: "../img/bilder/rainyspring_3.jpg",
+        overlay: "../img/overlay/rainyspring_3.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "150cm x 180cm",
+        index: 2,
+    },
+    {
+        name: "Sanktum",
+        kategorie: "Rainy Spring",
+        link: "../img/bilder/rainyspring_4.jpg",
+        overlay: "../img/overlay/rainyspring_4.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "120cm x 180cm",
+        index: 3,
+    },
+    {
+        name: "Der Baum",
+        kategorie: "Rainy Spring",
+        link: "../img/bilder/rainyspring_5.jpg",
+        overlay: "../img/overlay/rainyspring_5.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "150cm x 180cm",
+        index: 4,
+    },
+    {
+        name: "Nala",
+        kategorie: "Rainy Spring",
+        link: "../img/bilder/rainyspring_6.jpg",
+        overlay: "../img/overlay/rainyspring_6.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "120cm x 180cm",
+        index: 5,
+    },
+    {
+        name: "Die Wüste",
+        kategorie: "Rainy Spring",
+        link: "../img/bilder/rainyspring_7.jpg",
+        overlay: "../img/overlay/rainyspring_7.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "130cm x 180cm",
+        index: 6,
+    },
+    {
+        name: "Bardocks",
+        kategorie: "Rainy Spring",
+        link: "../img/bilder/rainyspring_8.jpg",
+        overlay: "../img/overlay/rainyspring_8.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "120cm x 180cm",
+        index: 7,
+    },
+];
+let winter = [
+    {
+        name: "The Art of Storytelling",
+        kategorie: "Winter",
+        link: "../img/bilder/winter_1.jpg",
+        overlay: "../img/overlay/winter_1.jpg",
+        stil: "Acryl & Kohle",
+        size: "150cm x 180cm",
+        index: 0,
+    },
+    {
+        name: "Frau Holle spielt mit der Welt",
+        kategorie: "Winter",
+        link: "../img/bilder/winter_2.jpg",
+        overlay: "../img/overlay/winter_2.jpg",
+        stil: "Acryl & Kohle",
+        size: "150cm x 180cm",
+        index: 0,
+    },
+    {
+        name: "Reset",
+        kategorie: "Winter",
+        link: "../img/bilder/winter_3.jpg",
+        overlay: "../img/overlay/winter_3.jpg",
+        stil: "Acryl auf Leinwand",
+        size: "120cm x 150cm",
+        index: 0,
+    },
+    {
+        name: "Doing Philosophy with Ravens",
+        kategorie: "Winter",
+        link: "../img/bilder/winter_2.jpg",
+        overlay: "../img/overlay/winter_2.jpg",
+        stil: "Acryl & Kohle",
+        size: "150cm x 180cm",
+        index: 0,
+    },
+];
+
+let categouries = ["magnificent", "dawn"];
+
+let gridImgs = Array.from(document.getElementsByClassName("gridItem"));
+let inner = Array.from(document.getElementsByClassName("inner"));
+let tempCat;
+
+console.log(gridImgs[0]);
+
+function fillGrid(i, kategorie) {
+    let rndm = Math.floor(Math.random() * kategorie.length);
+    gridImgs[i].style.backgroundImage = "url(" + kategorie[rndm].overlay + ")";
+    gridImgs[i].id = rndm;
+}
+
+// gridImgs[0].style.backgroundImage = "url(" + dawn[0].link + ")";
+
+fillGrid(0, magnificent);
+fillGrid(1, dawn);
+fillGrid(2, garden);
+fillGrid(3, heat);
+fillGrid(4, nature);
+fillGrid(5, layers);
+fillGrid(6, rainySpring);
+fillGrid(7, winter);
+fillGrid(8, dawn);
+// for (let i = 0; i < 3; i++) {
+//     fillGrid(i, dawn);
+// }
+// for (let i = 3; i < 5; i++) {
+//     fillGrid(i, garden);
+// }
+// for (let i = 5; i < 9; i++) {
+//     fillGrid(i, magnificent);
+// }
+
+// HOVER MAKER
+
+gridImgs.map((e, i) => {
+    e.addEventListener("mouseover", function () {
+        tempCat = e.style.backgroundImage.split("/")[3].split("_")[0];
+        console.log(tempCat);
+        this.children[0].classList.remove("slide-out-bottom");
+        this.children[0].classList.add("slide-in-bottom");
+
+        if (this.childElementCount == 1) {
+            let newDiv = document.createElement("div");
+            this.appendChild(newDiv);
+            if (tempCat == "dawn") {
+                this.children[1].style.backgroundImage =
+                    "url(" + dawn[e.id].link + ")";
+            } else if (tempCat == "magnificent") {
+                this.children[1].style.backgroundImage =
+                    "url(" + magnificent[e.id].link + ")";
+            } else if (tempCat == "garden") {
+                this.children[1].style.backgroundImage =
+                    "url(" + garden[e.id].link + ")";
+            } else if (tempCat == "heat") {
+                this.children[1].style.backgroundImage =
+                    "url(" + heat[e.id].link + ")";
+            } else if (tempCat == "nature") {
+                this.children[1].style.backgroundImage =
+                    "url(" + nature[e.id].link + ")";
+            } else if (tempCat == "layers") {
+                this.children[1].style.backgroundImage =
+                    "url(" + layers[e.id].link + ")";
+            } else if (tempCat == "winter") {
+                this.children[1].style.backgroundImage =
+                    "url(" + winter[e.id].link + ")";
+            } else if (tempCat == "rainyspring") {
+                this.children[1].style.backgroundImage =
+                    "url(" + rainySpring[e.id].link + ")";
+            }
+        }
+        this.children[1].classList.remove("fade-out");
+
+        this.children[1].classList.remove("imgHoverOut");
+        this.children[1].classList.add("imgHoverIn");
+        this.addEventListener("mouseleave", function () {
+            this.children[1].classList.remove("imgHoverIn");
+            this.children[1].classList.add("imgHoverOut");
+
+            this.children[0].classList.remove("slide-in-bottom");
+            this.children[0].classList.add("slide-out-bottom");
+            setTimeout(() => {
+                this.children[1].classList.add("fade-out");
+            }, 400);
+        });
+    });
+    // ON CLICK HREF HANDLING
+});
